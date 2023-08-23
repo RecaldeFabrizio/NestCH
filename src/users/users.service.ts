865d4cfe7932a1c9cff3eae487c745f3e867d40c
RecaldeFabrizio <fabrizio.recalde98@hotmail.com>
2023-08-23 01:08:30 -0300

    Merge branch 'main' of https://github.com/RecaldeFabrizio/EcommerceBackend

---
+++ src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+import {InjectModel} from '@nestjs/mongoose'
+import { User, UsersDocument } from './schema/users.schema';
+import { Model } from 'mongoose';
+ 
+@Injectable()
+export class UsersService {
+
+  constructor(@InjectModel(User.name) private userModel: Model<UsersDocument>){}
+
+  async create(createUserDto: CreateUserDto) {
+    return await this.userModel.create(createUserDto) 
+  }
+
+  async findAll() {
+    return await this.userModel.find()
+  }
+
+  async findOne(id: string) {
+    return await this.userModel.findById({_id: id});
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
